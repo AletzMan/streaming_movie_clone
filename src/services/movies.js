056152a5c7074d5118ceed0ba41319c0dd854f65
Alejandro Garcia Alonso <alejo_2986@hotmail.com>
2023-03-16 02:11:30 -0600

    Initial commit

---
+++ src/services/movies.js
@@ -0,0 +1,118 @@
+export const movie = {
+    "Title": "Captain Marvel",
+    "Year": "2019",
+    "Rated": "PG-13",
+    "Released": "08 Mar 2019",
+    "Runtime": "123 min",
+    "Genre": "Action, Adventure, Sci-Fi",
+    "Director": "Anna Boden, Ryan Fleck",
+    "Writer": "Anna Boden, Ryan Fleck, Geneva Robertson-Dworet",
+    "Actors": "Brie Larson, Samuel L. Jackson, Ben Mendelsohn",
+    "Plot": "Carol Danvers becomes one of the universe's most powerful heroes when Earth is caught in the middle of a galactic war between two alien races.",
+    "Language": "English",
+    "Country": "United States, Australia",
+    "Awards": "9 wins & 56 nominations",
+    "Poster": "https://m.media-amazon.com/images/M/MV5BMTE0YWFmOTMtYTU2ZS00ZTIxLWE3OTEtYTNiYzBkZjViZThiXkEyXkFqcGdeQXVyODMzMzQ4OTI@._V1_SX300.jpg",
+    "Ratings": [
+        {
+            "Source": "Internet Movie Database",
+            "Value": "6.8/10"
+        },
+        {
+            "Source": "Rotten Tomatoes",
+            "Value": "79%"
+        },
+        {
+            "Source": "Metacritic",
+            "Value": "64/100"
+        }
+    ],
+    "Metascore": "64",
+    "imdbRating": "6.8",
+    "imdbVotes": "570,134",
+    "imdbID": "tt4154664",
+    "Type": "movie",
+    "DVD": "28 May 2019",
+    "BoxOffice": "$426,829,839",
+    "Production": "N/A",
+    "Website": "N/A",
+    "Response": "True"
+}
+
+export const movieSearch = {
+    "Search": [
+    {
+    "Title": "Captain Marvel",
+    "Year": "2019",
+    "imdbID": "tt4154664",
+    "Type": "movie",
+    "Poster": "https://m.media-amazon.com/images/M/MV5BMTE0YWFmOTMtYTU2ZS00ZTIxLWE3OTEtYTNiYzBkZjViZThiXkEyXkFqcGdeQXVyODMzMzQ4OTI@._V1_SX300.jpg"
+    },
+    {
+    "Title": "Marvel One-Shot: Agent Carter",
+    "Year": "2013",
+    "imdbID": "tt3067038",
+    "Type": "movie",
+    "Poster": "https://m.media-amazon.com/images/M/MV5BZDIwZTM4M2QtMWFhYy00N2VmLWFlMjItMzI3NjBjYTc0OTMxXkEyXkFqcGdeQXVyNTE1NjY5Mg@@._V1_SX300.jpg"
+    },
+    {
+    "Title": "Marvel One-Shot: All Hail the King",
+    "Year": "2014",
+    "imdbID": "tt3438640",
+    "Type": "movie",
+    "Poster": "https://m.media-amazon.com/images/M/MV5BZGFkMTZkMDQtNzM4Yy00YWEwLTkzOWEtZTMyNDRlNmJhYWJhXkEyXkFqcGdeQXVyNTE1NjY5Mg@@._V1_SX300.jpg"
+    },
+    {
+    "Title": "Marvel One-Shot: Item 47",
+    "Year": "2012",
+    "imdbID": "tt2247732",
+    "Type": "movie",
+    "Poster": "https://m.media-amazon.com/images/M/MV5BMjNlMzAxNmQtOGEwZi00NTEyLWI0NWYtMTlhNmE2YTA3ZDVhXkEyXkFqcGdeQXVyNTE1NjY5Mg@@._V1_SX300.jpg"
+    },
+    {
+    "Title": "Marvel One-Shot: A Funny Thing Happened on the Way to Thor's Hammer",
+    "Year": "2011",
+    "imdbID": "tt2011109",
+    "Type": "movie",
+    "Poster": "https://m.media-amazon.com/images/M/MV5BYmVlYTg3N2QtMWM2OS00YWQyLWI2M2MtMDc0ZjBkZjk1MTY3XkEyXkFqcGdeQXVyNTE1NjY5Mg@@._V1_SX300.jpg"
+    },
+    {
+    "Title": "Marvel One-Shot: The Consultant",
+    "Year": "2011",
+    "imdbID": "tt2011118",
+    "Type": "movie",
+    "Poster": "https://m.media-amazon.com/images/M/MV5BNGE4YjU5MDAtYzYzMC00M2RlLTk0NDgtNDU1MjgyMGI0MjI3XkEyXkFqcGdeQXVyNTE1NjY5Mg@@._V1_SX300.jpg"
+    },
+    {
+    "Title": "Marvel Studios: Assembling a Universe",
+    "Year": "2014",
+    "imdbID": "tt3591568",
+    "Type": "movie",
+    "Poster": "https://m.media-amazon.com/images/M/MV5BZjNiN2NhYzQtYmI1NC00NGRmLWE2MWYtNjAxNjMzZmYxNDJhXkEyXkFqcGdeQXVyODQ4MjU1MDk@._V1_SX300.jpg"
+    },
+    {
+    "Title": "Pokémon the Movie: Volcanion and the Mechanical Marvel",
+    "Year": "2016",
+    "imdbID": "tt5889204",
+    "Type": "movie",
+    "Poster": "https://m.media-amazon.com/images/M/MV5BZTUwZDEzZGEtYTJlYi00OTRkLWJmMzItODU0ZWViMTUyYjUzXkEyXkFqcGdeQXVyNzEyMDQ1MDA@._V1_SX300.jpg"
+    },
+    {
+    "Title": "Adventures of Captain Marvel",
+    "Year": "1941",
+    "imdbID": "tt0033317",
+    "Type": "movie",
+    "Poster": "https://m.media-amazon.com/images/M/MV5BNjg0NTk3NjUyNF5BMl5BanBnXkFtZTgwNDQ5MjM1MjE@._V1_SX300.jpg"
+    },
+    {
+    "Title": "Marvel Rising: Secret Warriors",
+    "Year": "2018",
+    "imdbID": "tt7728344",
+    "Type": "movie",
+    "Poster": "https://m.media-amazon.com/images/M/MV5BZGU5YTVlZTktNzgzMS00MGVlLTgyMGMtNWYwNTkwNGY1MzllXkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_SX300.jpg"
+    }
+    ],
+    "totalResults": "186",
+    "Response": "True"
+    }
+
