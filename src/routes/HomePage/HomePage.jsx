2fca696081a4fe371b4b3db99fbcf56078767b47
Alejandro Garcia Alonso <alejo_2986@hotmail.com>
2023-03-18 15:59:39 -0600

    Added Section plans

--- src/routes/HomePage/HomePage.jsx
+++ src/routes/HomePage/HomePage.jsx
@@ -2,31 +2,27 @@ import React from 'react';
 import { HeaderHome } from './components/HeaderHome';
 import promo from '../../assets/promo.png';
 import { NavLink } from 'react-router-dom';
+import videoIntro from '../../assets/videos/intro-vid.mp4';
+import { Card } from './components/Card';
 
 function HomePage() {
     return (
-        <div>
+        <main className='homepage'>
             <HeaderHome />
             <section className='homepage__promo promo'>
-                <span>2023 LO TENDRÁ TODO</span>
-                <img src={promo} alt='ahorra tres meses' />
-                <span>CON EL PLAN ANUAL</span>
-                <NavLink className='homeheader__subscribe' to={''}>SUSCRÍBETE AHORA</NavLink>
-                <span>*aplica al plan prepago anual</span>
+                <video className='promo__video' src={videoIntro} type="video/mp4" muted autoPlay playsInline></video>
+                <span className='promo__title'>2023 LO TENDRÁ TODO</span>
+                <img className='promo__img' src={promo} alt='ahorra tres meses' />
+                <span className='promo__plan'>CON EL PLAN ANUAL</span>
+                <NavLink className='promo__subscribe button' to={''} data='SUSCRÍBETE AHORA'>SUSCRÍBETE AHORA</NavLink>
+                <span className='promo__restric'>*aplica al plan prepago anual</span>
             </section>
             <section className='homepage__plans plans'>
-                <h1>Elige tu plan</h1>
-                <button>¿QUÉ ESTÁ INCLUIDO?</button>
-                <div className='cardplan'>
-                    <span>AHORRA 3 MESES</span>
-                    <span>12 meses</span>
-                    <span>$1,249.00</span>
-                    <span>Igual a $104.08 al mes*</span>
-                    <NavLink>ELIGE ESTE PLAN</NavLink>
-                    <span>*Requiere pago anticipado. Ahorro basado en plan estándar anual vs plan estándar mensual por 12 meses.</span>
-                </div>
+                <h1 className='plans__title'>Elige tu plan</h1>
+                <button className='plans__button'>¿QUÉ ESTÁ INCLUIDO?</button>
+               <Card />
             </section>
-        </div>
+        </main>
     );
 }
 
