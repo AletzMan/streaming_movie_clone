b3b6e80174432980b2e09dcaf2c8fba13749171a
Alejandro Garcia Alonso <alejo_2986@hotmail.com>
2023-03-30 15:56:26 -0600

    Fixed route home

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig({   
+  base: "/streaming_movie_clone/",
   plugins: [react()],
 })
