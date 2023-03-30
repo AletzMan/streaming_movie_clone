6bd95576e75d809ee58039aec42954a22937106d
= <=>
2023-03-30 15:56:26 -0600

    Fixed route home

--- src/routes/PlanPickerPage/components/SectionPlans/HeaderPlans.jsx
+++ src/routes/PlanPickerPage/components/SectionPlans/HeaderPlans.jsx
@@ -5,9 +5,9 @@ const HeaderPlans = ({ login }) => {
     return (
         <header className='planspage'>
             <nav className='planspage__nav'>
-                <a href="/" className="planspage__link">
+                <NavLink to="/" className="planspage__link">
                     <img src={logo} alt="logo de streaming move ir a home" className="planspage__logo" />
-                </a>
+                </NavLink>
                 {login && <NavLink className='planspage__login' to='/login-page'>INICIAR SESIÓN</NavLink>}
             </nav>
         </header>
